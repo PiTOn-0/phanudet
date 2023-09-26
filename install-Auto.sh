619fb463955e37dad22e19027b8bb4a444149f83
lemon-zaza <107498835+lemon-zaza@users.noreply.github.com>
2023-09-26 09:17:53 +0700

    Update install-Auto.sh

--- install-Auto.sh
+++ install-Auto.sh
@@ -7,18 +7,18 @@ NC='\033[0m'
 /usr/bin/wget -q --no-check-certificate -O /usr/local/bin/os-vps https://raw.githubusercontent.com/lemon-zaza/phanudet/master/os-vps.sh > /dev/null 2>&1
 bash /usr/local/bin/os-vps
 
-lemon-zaza=$(/usr/local/bin/os-vps)
+VERSION_ID=$(/usr/local/bin/os-vps)
 
-if [ $lemon-zaza == "nono" ]; then
+if [ $VERSION_ID == "nono" ]; then
     echo -e "\033[1;31m สคริปต์ ไม่รองรับ!!${NC} "
     exit 1;
 fi
 
-if [ $lemon-zaza == "set01" ]; then
+if [ $VERSION_ID == "set01" ]; then
 wget -O Alone https://raw.githubusercontent.com/lemon-zaza/Alone/master/ubuntu18
 
 bash Alone
-elif [ $lemon-zaza == "set02" ]; then
+elif [ $VERSION_ID == "set02" ]; then
 wget -O Alone https://raw.githubusercontent.com/lemon-zaza/Alone/master/ubuntu20
 
 bash Alone
